c7998260bcacf8a6a30eb7b48264e0dae17ffb50
Rascol Radu <rascol.radu92@gmail.com>
2026-09-22 11:38:07 +0300

    Add shared app layout: header, collapsible sidebar, main outlet, footer

--- ui/src/app/app.routes.ts
+++ ui/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./pages/dashboard/dashboard').then((m) => m.Dashboard),
+  },
+  {
+    path: 'interviews',
+    loadComponent: () =>
+      import('./pages/interviews/interviews').then((m) => m.Interviews),
+  },
+];
---
+++ ui/src/app/layout/sidebar/sidebar.ts
@@ -0,0 +1,24 @@
+import { Component, inject } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Button } from '@openng/optimus-ui/button';
+import { LayoutService } from '../layout.service';
+
+interface NavItem {
+  label: string;
+  icon: string;
+  route: string;
+}
+
+@Component({
+  selector: 'app-sidebar',
+  imports: [RouterLink, RouterLinkActive, Button],
+  templateUrl: './sidebar.html',
+})
+export class Sidebar {
+  protected readonly layout = inject(LayoutService);
+
+  protected readonly navItems: NavItem[] = [
+    { label: 'Dashboard', icon: 'pi pi-home', route: '/dashboard' },
+    { label: 'Interviews', icon: 'pi pi-users', route: '/interviews' },
+  ];
+}
